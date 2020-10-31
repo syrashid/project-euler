572070a2ac5ceb55dc16f2bec277aa373c4f2787
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-31 21:06:09 +0300

    FEAT: Set up base tests

---
+++ multiples-of-3-and-5/lib/multiples.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+def multiples(int=1000)
+  # TODO: Return the sum of all multiples of 3 & 5 of the given integer
+
+end
---
+++ multiples-of-3-and-5/spec/multiples_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'multiples'
+
+describe '#multiples' do
+  it 'should return 23 when passed 10' do
+
+  end
+
+  it 'should return X when passed Y' do
+
+  end
+
+  it 'should return 0 when passed a negative value' do
+
+  end
+
+  it 'should raise an error if passed anything besides an integer' do
+
+  end
+end
