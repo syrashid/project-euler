8dab7a30481f83b5dbf7d387058fff1ef8767024
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-31 22:25:23 +0300

    FIX: Test made more verbose for argument error

--- multiples-of-3-and-5/lib/multiples.rb
+++ multiples-of-3-and-5/lib/multiples.rb
@@ -3,7 +3,7 @@
 def multiples(int=1000)
   # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
   # TODO: Find the sum of all the multiples of 3 or 5 below 1000.
-  raise ArgumentError unless int.is_a? Integer
+  raise ArgumentError, 'Argument is not an integer' unless int.is_a? Integer
 
   (0...int).inject(0) do |sum, n|
     ((n%3) == 0 || (n%5) == 0) ? (sum + n) : sum
