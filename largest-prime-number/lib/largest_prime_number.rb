50a5868c74f5de3ef9d7073a4261750eee4754c3
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-04 21:39:03 +0300

    FEAT: Made tests pass green

--- largest-prime-number/lib/largest_prime_number.rb
+++ largest-prime-number/lib/largest_prime_number.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
-def largest_prime_number(int)
+def largest_prime_number(int, aggregate = [])
   # The prime factors of 13195 are 5, 7, 13 and 29.
-  # TODO: What is the largest prime factor of the number 600851475143 ?
+  # TODO: What is the largest prime factor of the number 600851475143?
+  raise ArgumentError, 'Argument is not an integer' unless int.is_a? Integer
+  return 0 if int < 1
+
+  for i in 2..int
+    if int%i == 0
+      if int == i
+        aggregate << i
+      else
+        largest_prime_number(int/i, aggregate)
+        break
+      end
+    end
+  end
+  return aggregate.first
 end
