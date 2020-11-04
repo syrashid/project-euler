8e7a937a492968d0a3818d58f36f526885613849
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-04 21:12:58 +0300

    FEAT: Added tests for largest_prime_number function

--- largest-prime-number/spec/largest_prime_number_spec.rb
+++ largest-prime-number/spec/largest_prime_number_spec.rb
@@ -2,8 +2,21 @@
 
 require 'largest_prime_number'
 
-describe '#smoke_test' do
-  it 'should return true' do
-    expect(smoke_test).to be true
+describe '#largest_prime_number' do
+  it 'should return X when given Y' do
+    expect(largest_prime_number(13195)).to eq(29)
+  end
+
+  xit 'should return X when passed nothing or Y' do
+    expect(largest_prime_number(600851475143)).to eq(1)
+    expect(largest_prime_number(600851475143)).to eq(233_168)
+  end
+
+  it 'should return 0 when passed a negative value' do
+    expect(largest_prime_number(-10)).to eq(0)
+  end
+
+  it 'should raise an error if passed anything besides an integer' do
+    expect { largest_prime_number('abc') }.to raise_error(ArgumentError, 'Argument is not an integer')
   end
 end
