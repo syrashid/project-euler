b8c8fdb8ea697c0dfc57bd741012a7285bf67500
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-08 20:16:24 +0100

    FEAT: Added problem description

--- palindrome-product/lib/palindrome.rb
+++ palindrome-product/lib/palindrome.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-def palindrome
-  # TODO:
+def three_palindrome
+  # A palindromic number reads the same both ways.  The largest palindrome made from the product of two 2-digit number is 9009 = 91 x 99.
+  # TODO: Find the largist palindrome made from the product of two 3-digit numbers.
+
+end
+
+def two_palindrome
 
 end
--- palindrome-product/spec/palindrome_spec.rb
+++ palindrome-product/spec/palindrome_spec.rb
@@ -2,8 +2,18 @@
 
 require 'palindrome'
 
-describe '#palindrome' do
+describe '#three_palindrome' do
   it 'should return X' do
-    expect(palindrome).to eq(0)
+    expect(three_palindrome[:answer]).to eq(0)
+    # TODO: Implement array test
+    # expect(three_palindrome[:values]).to
+  end
+end
+
+describe '#two_palindrome' do
+  it 'should return X' do
+    expect(two_palindrome[:answer]).to eq(9009)
+    # TODO: Implement array test
+    # expect(two_palindrome[:values]).to
   end
 end
