e0d515c1bb30f56367c946f1bfa50b2891548235
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-09 10:12:46 +0100

    FIX: Assertion language

--- palindrome-product/spec/palindrome_spec.rb
+++ palindrome-product/spec/palindrome_spec.rb
@@ -3,14 +3,14 @@
 require 'palindrome'
 
 describe '#three_palindrome' do
-  it 'should return 906609' do
+  it 'should return 906609 for an answer and the values 913 and 993' do
     expect(three_palindrome[:answer]).to eq(906609)
     expect(three_palindrome[:values]).to include(913, 993)
   end
 end
 
 describe '#two_palindrome' do
-  it 'should return 9009' do
+  it 'should return 9009 for an answer and the values 91 and 99' do
     expect(two_palindrome[:answer]).to eq(9009)
     expect(two_palindrome[:values]).to include(91, 99)
   end
