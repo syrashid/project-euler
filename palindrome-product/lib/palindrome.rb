7103f5032e7071e277a12f0b57240633fe8e94a6
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-09 10:17:00 +0100

    FIX: All rubocop issues

--- palindrome-product/lib/palindrome.rb
+++ palindrome-product/lib/palindrome.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 def three_palindrome
-  # A palindromic number reads the same both ways.  The largest palindrome made from the product of two 2-digit number is 9009 = 91 x 99.
+  # A palindromic number reads the same both ways.
+  # The largest palindrome made from the product of two 2-digit number is 9009 = 91 x 99.
   # TODO: Find the largist palindrome made from the product of two 3-digit numbers.
   answer_hash = { answer: 0 }
   (0..899).each do |i|
@@ -21,8 +22,7 @@ def two_palindrome
     (0..89).each do |j|
       product = (99 - i) * (99 - j)
       if palindromic_number?(product) && product > answer_hash[:answer]
-        answer_hash[:answer] = product
-        answer_hash[:values] = [99 - i, 99 - j]
+        answer_hash.merge!(answer: product, values: [99 - i, 99 - j])
       end
     end
   end
