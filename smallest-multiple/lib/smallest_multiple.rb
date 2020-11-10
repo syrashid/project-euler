1b8bf5f1f565b9b535ea34f61d271a5694405c4a
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-10 23:00:16 +0100

    ARCH: Copied over base template and set up for code

---
+++ smallest-multiple/lib/smallest_multiple.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+def smallest_multiple
+  # TODO: Return true
+  true
+end
---
+++ smallest-multiple/spec/file_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'smallest_multiple'
+
+describe '#smallest_multiple' do
+  it 'should return true' do
+    expect(smallest_multiple).to be true
+  end
+end
