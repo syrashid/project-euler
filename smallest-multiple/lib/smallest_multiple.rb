10e5e7ed4dbcfcaff431e05a1f0c71e354a9c68a
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-10 23:18:16 +0100

    FIX: Style and refactor

--- smallest-multiple/lib/smallest_multiple.rb
+++ smallest-multiple/lib/smallest_multiple.rb
@@ -5,6 +5,6 @@ def smallest_multiple
   # TODO: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
   seed = 2520 # seed is based off above description
-  seed += 2520 until (1..20).to_a.all? { |i| (seed % i).zero? }
+  (seed += 2520) until (1..20).to_a.all? { |i| (seed % i).zero? }
   seed
 end
