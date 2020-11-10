cd15150fbfa40651f474e733078fafd7d62c1e50
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-10 23:14:40 +0100

    FIX: Improved test speed and structure

--- smallest-multiple/lib/smallest_multiple.rb
+++ smallest-multiple/lib/smallest_multiple.rb
@@ -6,13 +6,8 @@ def smallest_multiple
   searching = true
   seed = 2520 # seed is based off above description
   while searching
-    # Iterate by 20 starting with seed
-    all_divisible = (1..20).to_a.all? do |i|
-      # For each number divide by 20 to 1 and check for divisibility
-      (seed % i).zero?
-    end
-    # Set search to false when found
-    all_divisible ? searching = false : seed += 20
+    all_divisible = (1..20).to_a.all? { |i| (seed % i).zero? }
+    all_divisible ? searching = false : seed += 2520
   end
   seed
 end
