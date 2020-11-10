6fac86d99fd44f9b77c4adc779f2b1ec9074815f
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-10 23:09:31 +0100

    FEAT: First pass at test

--- smallest-multiple/lib/smallest_multiple.rb
+++ smallest-multiple/lib/smallest_multiple.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 def smallest_multiple
-  # TODO: Return true
-  true
+  # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  # TODO: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  searching = true
+  seed = 2520 # seed is based off above description
+  while searching
+    # Iterate by 20 starting with seed
+    all_divisible = (1..20).to_a.all? do |i|
+      # For each number divide by 20 to 1 and check for divisibility
+      (seed % i).zero?
+    end
+    # Set search to false when found
+    all_divisible ? searching = false : seed += 20
+  end
+  seed
 end
