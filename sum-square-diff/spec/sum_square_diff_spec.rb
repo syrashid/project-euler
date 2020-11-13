e381f1e412187264f6b85349ee3632929871bf05
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-13 10:29:44 +0100

    FIX: Fixed test with proper value

--- sum-square-diff/spec/sum_square_diff_spec.rb
+++ sum-square-diff/spec/sum_square_diff_spec.rb
@@ -3,7 +3,7 @@
 require 'sum_square_diff'
 
 describe '#sum_square_diff' do
-  it 'should return X' do
-    expect(sum_square_diff).to eq(0)
+  it 'should return 25_164_150' do
+    expect(sum_square_diff).to eq(25_164_150)
   end
 end
