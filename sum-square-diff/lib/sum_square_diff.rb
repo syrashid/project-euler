5101dad347e5268ca8c18764ce97b60affba90e5
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-13 10:30:51 +0100

    FIX: Style offenses corrected

--- sum-square-diff/lib/sum_square_diff.rb
+++ sum-square-diff/lib/sum_square_diff.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 def sum_square_diff
-  # TODO: Return true
-  sum_squares = (1..100).to_a.sum { |val| val ** 2 }
-  square_sum = ((1..100).to_a.sum ** 2)
+  # TODO: Find the difference between the sum of the squares of the first
+  # one hundred natural numbers and the square of the sum.
+  sum_squares = (1..100).to_a.sum { |val| val**2 }
+  square_sum = ((1..100).to_a.sum**2)
   square_sum - sum_squares
 end
