83b0d0a5db80ee1b64cf96e8870e560caa1da478
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-13 10:36:26 +0100

    FEAT: Added additional tests and a limit parameter

--- sum-square-diff/lib/sum_square_diff.rb
+++ sum-square-diff/lib/sum_square_diff.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-def sum_square_diff
+def sum_square_diff(limit = 100)
   # TODO: Find the difference between the sum of the squares of the first
   # one hundred natural numbers and the square of the sum.
-  sum_squares = (1..100).to_a.sum { |val| val**2 }
-  square_sum = ((1..100).to_a.sum**2)
+
+  # Brute force method
+  sum_squares = (1..limit).to_a.sum { |val| val**2 }
+  square_sum = ((1..limit).to_a.sum**2)
   square_sum - sum_squares
 end
--- sum-square-diff/spec/sum_square_diff_spec.rb
+++ sum-square-diff/spec/sum_square_diff_spec.rb
@@ -3,7 +3,11 @@
 require 'sum_square_diff'
 
 describe '#sum_square_diff' do
-  it 'should return 25_164_150' do
+  it 'should return 25_164_150 when passed nothing or 100' do
     expect(sum_square_diff).to eq(25_164_150)
+  end
+
+  it 'should return 2640 when passed 10' do
+    expect(sum_square_diff(10)).to eq(2640)
   end
 end
