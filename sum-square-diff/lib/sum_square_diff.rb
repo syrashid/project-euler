0863b8c97655cfa590489cdfc34bba2343e8a342
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-13 10:39:25 +0100

    FEAT: Implemented second method for solve

--- sum-square-diff/lib/sum_square_diff.rb
+++ sum-square-diff/lib/sum_square_diff.rb
@@ -5,7 +5,12 @@ def sum_square_diff(limit = 100)
   # one hundred natural numbers and the square of the sum.
 
   # Brute force method
-  sum_squares = (1..limit).to_a.sum { |val| val**2 }
-  square_sum = ((1..limit).to_a.sum**2)
-  square_sum - sum_squares
+  # sum_squares = (1..limit).to_a.sum { |val| val**2 }
+  # square_sum = ((1..limit).to_a.sum**2)
+  # square_sum - sum_squares
+
+  # Elegant solution
+  sum = limit * (limit + 1) / 2
+  sum_square = (2 * limit + 1) * (limit + 1) * limit / 6
+  sum**2 - sum_square
 end
