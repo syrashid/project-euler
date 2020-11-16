efee2f9d81ac7108571615d1c1c5e8cc25525719
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 08:03:49 +0100

    FEAT: Set up for potential future soln

--- 10001st-prime/lib/last_prime.rb
+++ 10001st-prime/lib/last_prime.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 def last_prime(index)
-  # TODO: Return true
-  n = (index/2.0).ceil - 1
-  index.odd? ? (6*n) - 1 : (6*n) + 1
+  # Look up Sieve of Eratosthenes
 end
