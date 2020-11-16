47fee8ca202e59e863a5dfabb53929f0970786e3
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-16 07:40:23 +0100

    FEAT: Got base tests pass

--- 10001st-prime/lib/last_prime.rb
+++ 10001st-prime/lib/last_prime.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-def last_prime
+def last_prime(index)
   # TODO: Return true
-  true
+  n = (index/2.0).ceil - 1
+  index.odd? ? (6*n) - 1 : (6*n) + 1
 end
--- 10001st-prime/spec/last_prime_spec.rb
+++ 10001st-prime/spec/last_prime_spec.rb
@@ -3,7 +3,11 @@
 require 'last_prime'
 
 describe '#last_prime' do
-  it 'should return X' do
-    expect(last_prime).to be true
+  it 'should return 11 when passed 5' do
+    expect(last_prime(5)).to eq(11)
+  end
+
+  it 'should return 13 when passed 6' do
+    expect(last_prime(6)).to eq(13)
   end
 end
