d785e46f701d6649b3d719e10639ebdd9e72c999
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-25 07:00:59 +0100

    FIX: Fixed up tests

--- 10001st-prime/lib/last_prime.rb
+++ 10001st-prime/lib/last_prime.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-def last_prime(index)
+def last_prime(index = 10_001)
   # Look up Sieve of Eratosthenes
 end
--- 10001st-prime/spec/last_prime_spec.rb
+++ 10001st-prime/spec/last_prime_spec.rb
@@ -11,7 +11,8 @@ describe '#last_prime' do
     expect(last_prime(6)).to eq(13)
   end
 
-  it 'should return X when passed nothing or 10001' do
-    expect(last_prime(10001)).to eq(13)
+  it 'should return X when passed nothing or 10_001' do
+    expect(last_prime(10_001)).to eq(13)
+    expect(last_prime).to eq(13)
   end
 end
