735df6c93367d1bdbdcc606522c8f8eaf06b7b14
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-25 08:13:10 +0100

    FIX: Style fixes

--- 10001st-prime/lib/last_prime.rb
+++ 10001st-prime/lib/last_prime.rb
@@ -1,23 +1,7 @@
 # frozen_string_literal: true
 
 def last_prime(index = 10_001)
-  # SET UP PRIME NUMBER SIEVE (OF ERATOSTHENES)
-
-  # Set an upper limit on how many numbers to sieve
-  upper_lim = 150_000
-  # Generate list of booleans up to upper limit
-  prime_bools = Array.new(upper_lim, true)
-  # Create range of numbers we'll use to iterate over booleans
-  test_vals = (2..Math.sqrt(upper_lim).floor).to_a
-  # Iterate over test_vals and set prime_indices to false based off test_vals
-  test_vals.each do |val|
-    if prime_bools[val]
-      multipliers = (2..upper_lim/2).to_a
-      multipliers.each do |m|
-        prime_bools[val*m] = false
-      end
-    end
-  end
+  prime_bools = generate_prime_bools
 
   # ASSOCIATE POSITION WITH PRIME NUMBER INDICES
   prime_nums = []
@@ -28,3 +12,21 @@ def last_prime(index = 10_001)
   prime_nums[index + 1]
 end
 
+def generate_prime_bools
+  # SET UP PRIME NUMBER SIEVE (OF ERATOSTHENES)
+
+  # Set an upper limit on how many numbers to sieve
+  upper_lim = 150_000
+  # Generate list of booleans up to upper limit
+  prime_bools = Array.new(upper_lim, true)
+  # Create range of numbers we'll use to iterate over booleans
+  test_vals = (2..Math.sqrt(upper_lim).floor).to_a
+  # Iterate over test_vals and set prime_indices to false based off test_vals
+  test_vals.each do |val|
+    next unless prime_bools[val]
+
+    multipliers = (2..upper_lim / 2).to_a
+    multipliers.each { |m| prime_bools[val * m] = false }
+  end
+  prime_bools
+end
--- 10001st-prime/spec/last_prime_spec.rb
+++ 10001st-prime/spec/last_prime_spec.rb
@@ -11,8 +11,8 @@ describe '#last_prime' do
     expect(last_prime(6)).to eq(13)
   end
 
-  it 'should return X when passed nothing or 10_001' do
-    expect(last_prime(10_001)).to eq(104743)
-    expect(last_prime).to eq(104743)
+  it 'should return 104_743 when passed nothing or 10_001' do
+    expect(last_prime(10_001)).to eq(104_743)
+    expect(last_prime).to eq(104_743)
   end
 end
